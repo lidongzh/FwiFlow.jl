98d57a702181265afb0b47dfd941ead07770f794
Kailai Xu <kailaix@stanford.edu>
2019-04-05 16:28:49 -0400

    update

--- src/simple.jl
+++ src/simple.jl
@@ -8,9 +8,9 @@ include("poisson_op.jl")
 # Solver parameters
 m = 20
 n = 20
-h = 50.0 # 50 meters
-T = 8640000 # 100 days
-NT = 100
+h = 1.0 # 50 meters
+T = 0.01 # 100 days
+NT = 1000
 Δt = T/NT 
 x = (1:m)*h|>collect
 z = (1:n)*h|>collect
@@ -33,12 +33,12 @@ end
 function Kro(So)
     return So ^2
 end
-ρw = constant(1014.0)
-ρo = constant(787.3075)
-μw = constant(0.001)
-μo = constant(0.002)
-K = Variable(5.9215e-14 .*ones(m,n))
-g = constant(9.8)
+ρw = constant(1.)
+ρo = constant(1.)
+μw = constant(1.)
+μo = constant(1.)
+K = Variable(ones(m,n))
+g = constant(1.0)
 ϕ = Variable(0.25 .* ones(m,n))
 
 function geto(o::Union{Array,PyObject}, i::Int64, j::Int64)
@@ -61,9 +61,9 @@ end
 
 function G(f, p)
     f1 = (geto(f, 0, 0) + geto(f, 1, 0))/2
-    f2 = (geto(f, -1, 0)+geto(f, 0, 0))/2
+    f2 = (geto(f, -1, 0) + geto(f, 0, 0))/2
     f3 = (geto(f,0,1) + geto(f,0,0))/2
-    f4 = (geto(f,0,-1)+geto(f,0,0))/2
+    f4 = (geto(f,0,-1) + geto(f,0,0))/2
     rhs = -f1*(geto(p,1,0)-geto(p,0,0)) +
             f2*(geto(p,0,0)-geto(p,-1,0)) -
             f3*(geto(p,0,1)-geto(p,0,0)) +
@@ -82,21 +82,20 @@ end
 
 # variables : sw, u, v, p
 # (time dependent) parameters: qw, qo, ϕ
-function onestep(sw, p, qw, qo)
+function onestep(sw, qw, qo)
     # step 1: update p
     λw = Krw(sw)/μw
     λo = Kro(1-sw)/μo
     λ = λw + λo
     f = λw/λ
     q = qw + qo
-    Pc = zeros(m, n)
     Θ = G((λw*ρw+λo*ρo)*g, Z)
-    p = poisson_op(λ*K, Θ+q, constant(h))
+    p = poisson_op(λ.*K, Θ+q, constant(h))
 
 
     # step 2: update u, v
-    rhs_u = -geto(K, 0, 0).*geto(λ, 0, 0)/h*(geto(p, 1, 0) - geto(p, 0, 0))
-    rhs_v = -geto(K, 0, 0).*geto(λ, 0, 0)/h*(geto(p, 0, 1) - geto(p, 0, 0)) +
+    rhs_u = -geto(K, 0, 0).*geto(λ, 0, 0)/h.*(geto(p, 1, 0) - geto(p, 0, 0))
+    rhs_v = -geto(K, 0, 0).*geto(λ, 0, 0)/h.*(geto(p, 0, 1) - geto(p, 0, 0)) +
             geto(K, 0, 0).*geto(λw*ρw+λo*ρo, 0, 0)*g
     u = constant(zeros(m, n))
     v = constant(zeros(m, n))
@@ -104,11 +103,11 @@ function onestep(sw, p, qw, qo)
     v = scatter_add(v, 2:m-1, 2:n-1, rhs_v)
 
     # step 3: update sw
-    rhs = geto(qw, 0, 0) - (geto(f, 1, 0)-geto(f, 0, 0))/h*geto(u, 0, 0) -
-            (geto(f, 0, 1)-geto(f, 0, 0))/h*geto(v, 0, 0) -
-            geto(f, 0, 0) * (geto(u, 0, 0)-geto(u, -1, 0)/h + (geto(v, 0, 0)-geto(v, 0, -1))/h) -
+    rhs = geto(qw, 0, 0) - (geto(f, 1, 0)-geto(f, 0, 0))/h.*geto(u, 0, 0) -
+            (geto(f, 0, 1)-geto(f, 0, 0))/h.*geto(v, 0, 0) -
+            geto(f, 0, 0) .* ( (geto(u, 0, 0)-geto(u, -1, 0))/h + (geto(v, 0, 0)-geto(v, 0, -1))/h) -
             geto(G(K.*f*λo*(ρw-ρo)*g, Z), 0, 0)
-    rhs = Δt*rhs/geto(ϕ, 0, 0) + geto(sw, 0, 0)
+    rhs = Δt*rhs/geto(ϕ, 0, 0)
     sw = scatter_add(sw, 2:m-1, 2:n-1, rhs)
     return sw, p
 end
@@ -121,7 +120,7 @@ Solve the two phase flow equation.
 `qw` and `qo` -- `NT x m x n` numerical array, `qw[i,:,:]` the corresponding value of qw at i*Δt
 `sw0` and `p0` -- initial value for `sw` and `p`. `m x n` numerical array.
 """
-function solve(qw, qo, sw0, p0)
+function solve(qw, qo, sw0)
     qw_arr = constant(qw) # qw: NT x m x n array
     qo_arr = constant(qo)
     function condition(i, tas...)
@@ -129,17 +128,13 @@ function solve(qw, qo, sw0, p0)
     end
     function body(i, tas...)
         ta_sw, ta_p = tas
-        sw, p = onestep(read(ta_sw, i), read(ta_p, i), qw_arr[i], qo_arr[i])
+        sw, p = onestep(read(ta_sw, i), qw_arr[i], qo_arr[i])
         ta_sw = write(ta_sw, i+1, sw)
         ta_p = write(ta_p, i+1, p)
         i+1, ta_sw, ta_p
     end
-    # function body(i, tas...)
-    #     i+1, tas[1], tas[2]
-    # end
     ta_sw, ta_p = TensorArray(NT+1), TensorArray(NT+1)
     ta_sw = write(ta_sw, 1, constant(sw0))
-    ta_p = write(ta_p, 1, constant(p0))
     i = constant(1, dtype=Int32)
     _, ta_sw, ta_p = while_loop(condition, body, [i; ta_sw; ta_p])
     out_sw, out_p = stack(ta_sw), stack(ta_p)
@@ -154,6 +149,19 @@ function vis(val, args...;kwargs...)
     end
 end
 
+qw = zeros(NT, m, n)
+qw[:,15,5] .= 1.0
+qo = zeros(NT, m, n)
+sw0 = zeros(m, n)
+out_sw, out_p = solve(qw, qo, sw0)
+
+
+sess = Session(); init(sess)
+S, P = run(sess, [out_sw, out_p])
+error("stop")
+
+
+#=
 # Step 1: Assign numerical values to qw, qo, sw0, p0
 # qw = 
 # qo = 
@@ -175,3 +183,4 @@ sw, p = run(sess, [out_sw, out_p])
 
 # # Step 4: Visualize
 # vis(sw)
+=#
