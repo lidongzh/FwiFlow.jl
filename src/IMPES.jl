1c7d56d823ac519f36484d9280efba83334c898b
lidongzh <lidongzh@stanford.edu>
2019-04-05 21:22:22 -0700

    IMPES

--- src/IMPES.jl
+++ src/IMPES.jl
@@ -6,26 +6,28 @@ np = pyimport("numpy")
 include("poisson_op.jl")
 
 # Solver parameters
+# m = 20
+# n = 20
+# h = 1.0 # 50 meters
+# T = 25 # 100 days
+# NT = 25
+# Δt = 0.7/NT 
+# x = (1:m)*h|>collect
+# z = (1:n)*h|>collect
+# X, Z = np.meshgrid(x, z)
 m = 20
 n = 20
-h = 1.0 # 50 meters
-T = 0.01 # 100 days
-NT = 1000
-Δt = T/NT
-x = (1:m)*h|>collect
-z = (1:n)*h|>collect
+h = 20.0
+# T = 100 # 100 days
+# NT = 100
+# Δt = T/(NT+1)
+NT = 20
+Δt = 1
+# T = NT() # 100 days
+z = (1:m)*h|>collect
+x = (1:n)*h|>collect
 X, Z = np.meshgrid(x, z)
 
-
-# todo 
-#=
-Krw, Kro -- function mxn tensor to mxn tensor
-μw, μo -- known mxn
-ρw, ρo -- known mxn
-K -- scalar
-g -- constant ≈ 9.8?
-ϕ -- known mxn
-=#
 function Krw(Sw)
     return Sw ^ 2
 end
@@ -33,13 +35,13 @@ end
 function Kro(So)
     return So ^2
 end
-ρw = constant(1.)
-ρo = constant(1.)
-μw = constant(1.)
-μo = constant(1.)
-K = Variable(ones(m,n))
-g = constant(1.0)
-ϕ = Variable(0.25 .* ones(m,n))
+ρw = constant(996.9571)
+ρo = constant(640.7385)
+μw = constant(1e-3)
+μo = constant(1e-3)
+K = Variable(9.8692e-14*ones(m,n))
+g = constant(0.0)
+ϕ = Variable(0.25*ones(m,n))
 
 function geto(o::Union{Array,PyObject}, i::Int64, j::Int64)
     if i==-1
@@ -75,9 +77,19 @@ function G(f, p)
     else
         q = constant(zeros(m, n))
         q = scatter_add(q, 2:m-1, 2:n-1, rhs/h^2)
-        # q[2:m-1, 2:n-1] += rhs/h^2
     end
     q
+end
+
+
+function ave_normal(quantity, m, n)
+    aa = 0.0
+    for i = 1:m
+        for j = 1:n
+            aa = aa + quantity[i,j]
+        end
+    end
+    return aa/(m*n)
 end
 
 
@@ -90,14 +102,14 @@ function onestep(sw, qw, qo)
     λ = λw + λo
     f = λw/λ
     q = qw + qo
-    Θ = G((λw*ρw+λo*ρo)*g, Z)
-    p = poisson_op(λ.*K, Θ+q, constant(h))
-
+    Θ = G(K.*(λw*ρw+λo*ρo)*g, Z)
+    load_normal = (Θ+q) - ave_normal(Θ+q,m,n)
+    p = poisson_op(λ.*K, load_normal, constant(h))
 
     # step 2: update u, v
-    rhs_u = -(geto(K, 0, 0)+geto(K,1,0))/2.0 .* (geto(λ, 0, 0) + get(λ, 1, 0))/2h .* (geto(p, 1, 0) - geto(p, 0, 0))
-    rhs_v = -(geto(K, 0, 0)+geto(K,0,1))/2.0 .* (geto(λ, 0, 0) + get(λ, 0, 1))/2h .* (geto(p, 0, 1) - geto(p, 0, 0)) +
-            (geto(K, 0, 0)+geto(K,0,1))/2.0 .* (geto(λw*ρw+λo*ρo, 0, 0)+geto(λw*ρw+λo*ρo, 0, 1))/2 * g
+    rhs_u = -geto(K, 0, 0).*geto(λ, 0, 0)/h.*(geto(p, 1, 0) - geto(p, 0, 0))
+    rhs_v = -geto(K, 0, 0).*geto(λ, 0, 0)/h.*(geto(p, 0, 1) - geto(p, 0, 0)) +
+            geto(K, 0, 0).*geto(λw*ρw+λo*ρo, 0, 0)*g
     u = constant(zeros(m, n))
     v = constant(zeros(m, n))
     u = scatter_add(u, 2:m-1, 2:n-1, rhs_u)
@@ -106,13 +118,11 @@ function onestep(sw, qw, qo)
     # step 3: update sw
     rhs = geto(qw, 0, 0) - (geto(f, 1, 0)-geto(f, 0, 0))/h.*geto(u, 0, 0) -
             (geto(f, 0, 1)-geto(f, 0, 0))/h.*geto(v, 0, 0) -
-            geto(f, 0, 0) .* ( 
-                (geto(u, 0, 0)-geto(u, -1, 0))/h + (geto(v, 0, 0)-geto(v, 0, -1))/h
-            ) -
-            geto(G(K.*f.*λo*(ρw-ρo)*g, Z), 0, 0)
+            geto(f, 0, 0) .* ( (geto(u, 0, 0)-geto(u, -1, 0))/h + (geto(v, 0, 0)-geto(v, 0, -1))/h) -
+            geto(G(K.*f*λo*(ρw-ρo)*g, Z), 0, 0)
     rhs = Δt*rhs/geto(ϕ, 0, 0)
     sw = scatter_add(sw, 2:m-1, 2:n-1, rhs)
-    return sw, p
+    return sw, p, u, v, f
 end
 
 
@@ -130,17 +140,22 @@ function solve(qw, qo, sw0)
         i <= NT
     end
     function body(i, tas...)
-        ta_sw, ta_p = tas
-        sw, p = onestep(read(ta_sw, i), qw_arr[i], qo_arr[i])
+        println(i)
+        ta_sw, ta_p, ta_u, ta_v, ta_f = tas
+        sw, p, u, v, f = onestep(read(ta_sw, i), qw_arr[i], qo_arr[i])
         ta_sw = write(ta_sw, i+1, sw)
         ta_p = write(ta_p, i+1, p)
-        i+1, ta_sw, ta_p
+        ta_u = write(ta_u, i+1, u)
+        ta_v = write(ta_v, i+1, v)
+        ta_f = write(ta_f, i+1, f)
+        i+1, ta_sw, ta_p, ta_u, ta_v, ta_f
     end
     ta_sw, ta_p = TensorArray(NT+1), TensorArray(NT+1)
+    ta_u, ta_v, ta_f = TensorArray(NT+1), TensorArray(NT+1), TensorArray(NT+1)
     ta_sw = write(ta_sw, 1, constant(sw0))
     i = constant(1, dtype=Int32)
-    _, ta_sw, ta_p = while_loop(condition, body, [i; ta_sw; ta_p])
-    out_sw, out_p = stack(ta_sw), stack(ta_p)
+    _, ta_sw, ta_p, ta_u, ta_v, ta_f = while_loop(condition, body, [i; ta_sw; ta_p; ta_u; ta_v; ta_f])
+    out_sw, out_p, out_u, out_v, ta_f = stack(ta_sw), stack(ta_p), stack(ta_u), stack(ta_v), stack(ta_f)
 end
 
 function vis(val, args...;kwargs...)
@@ -154,15 +169,17 @@ function vis(val, args...;kwargs...)
 end
 
 qw = zeros(NT, m, n)
-qw[:,15,5] .= 1.0
+qw[:,5, 5] .= 0.0026
 qo = zeros(NT, m, n)
+qo[:,5, 15] .= -0.004
 sw0 = zeros(m, n)
-out_sw, out_p = solve(qw, qo, sw0)
+out_sw, out_p, out_u, out_v, out_f = solve(qw, qo, sw0)
 
 
 sess = Session(); init(sess)
-S, P = run(sess, [out_sw, out_p])
+S, P, U, V, F = run(sess, [out_sw, out_p, out_u, out_v, out_f])
 vis(S)
+# vis(U)
 # figure()
 # vis(P)
 error("stop")
@@ -179,12 +196,15 @@ qw[:,15,5] .= 0.0018
 qo = zeros(NT, m, n)
 sw0 = zeros(m, n)
 p0 = 3.0337e+07*ones(m,n)
+
 # # Step 2: Construct Graph
 out_sw, out_p = solve(qw, qo, sw0, p0)
+
 # # Step 3: Run
 sess = Session()
 init(sess)
 sw, p = run(sess, [out_sw, out_p])
+
 # # Step 4: Visualize
 # vis(sw)
 =#
