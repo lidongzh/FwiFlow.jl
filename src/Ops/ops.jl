cff9e99ff4a2f10373209192552d43d56c977b0f
Kailai Xu <kailaix@stanford.edu>
2019-12-07 18:19:03 -0800

    fix ops.jl

--- src/Ops/ops.jl
+++ src/Ops/ops.jl
@@ -20,35 +20,6 @@ function laplacian(args...)
     laplacian(args...)
 end
 
-
-h = 1.0
-rho = 1000.0
-G = 9.8
-len_z = 16
-len_x = 32
-nz = Int(len_z/h + 1)
-nx = Int(len_x/h + 1)
-tf_h=constant(1.0)
-coef = zeros(nz, nx)
-rhs  = zeros(nz, nx)
-for i = 1:nz
-    for j = 1:nx
-        rhs[i,j] = -sin(2*pi/len_z*(i-1)*h) * sin(2*pi/len_x*(j-1)*h)
-        coef[i,j] = 1.0 - cos(2*pi/len_z*(i-1)*h) * sin(2*pi/len_x*(j-1)*h) * len_z / (2*pi*rho*G)
-
-        # rhs[i,j] = 2.0*(i-1)*h*exp(-(((i-1)*h)^2) -(((j-1)*h)^2)) * rho * G
-        # coef[i,j] = 1.0 + exp(-(((i-1)*h)^2) -(((j-1)*h)^2))
-    end
-end
-
-tf_coef = constant(coef)
-tf_rhs = constant(rhs)
-function scalar_function(m)
-    return sum(tanh(poisson_op(tf_coef,m,tf_h,constant(rho*G), constant(0))))
-    # return sum(tanh(poisson_op(m,tf_rhs,tf_h, constant(rho*G), constant(0))))
-end
-
-
 @doc raw"""
     poisson_op(c::Union{PyObject, Float64}, g::Union{PyObject, Float64}, 
         h::Union{PyObject, Float64}, ρ::Union{PyObject, Float64}, index::Union{Integer, PyObject}=0)
