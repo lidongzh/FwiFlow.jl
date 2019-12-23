2a9627cee447d1925d25d5a199e784fab69582a3
Kailai Xu <kailaix@stanford.edu>
2019-12-23 16:26:18 +0800

    fix bug

--- test/ACE.sh
+++ test/ACE.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=ACE
 #SBATCH --nodes=1 --ntasks=1
-#SBATCH --time=5:00
 #SBATCH --output=ACE_%A_%a.out
 #SBATCH --error=ACE_%A_%a.err
-#SBATCH --partition cpu
+#SBATCH --partition CPU
 #SBATCH --array=1-8
 echo "$SLURM_ARRAY_TASK_ID"
 
 dt=(0 0 0 0 1 1 1 1)
 ss=(0.5 0.1 0.05 0.01 0.5 0.1 0.05 0.01)
 
-echo julia ACE.jl 0 ${dt[$SLURM_ARRAY_TASK_ID]} ${ss[$SLURM_ARRAY_TASK_ID]}
-echo julia ACE.jl 1 ${dt[$SLURM_ARRAY_TASK_ID]} ${ss[$SLURM_ARRAY_TASK_ID]}
+julia ACE.jl 0 ${dt[$SLURM_ARRAY_TASK_ID]} ${ss[$SLURM_ARRAY_TASK_ID]}
+julia ACE.jl 1 ${dt[$SLURM_ARRAY_TASK_ID]} ${ss[$SLURM_ARRAY_TASK_ID]}
 
