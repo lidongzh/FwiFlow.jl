ab899fbb9f4d9a5a944314feb8ae6579baa5ebfd
Kailai Xu <kailaix@stanford.edu>
2020-03-13 20:39:01 -0700

    fix doc

--- src/utils.jl
+++ src/utils.jl
@@ -186,7 +186,7 @@ Adds PML boundaries to `cp`, `cs` and `den`.
 The original `nz_orig x nx_orig` grid is resampled to `nz x nx`. 
 
 !!! note 
-		`nPad` is used to make the number of nodes in the $z$ direction a multiple of 32 (for coalesced memory access). 
+	`nPad` is used to make the number of nodes in the $z$ direction a multiple of 32 (for coalesced memory access). 
 """
 function padding(cp, cs, den, nz_orig, nx_orig, nz, nx, nPml, nPad)
 	tran_cp = tf.reshape(cp, (1, nz_orig, nx_orig, 1))
